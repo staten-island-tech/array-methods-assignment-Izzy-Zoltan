9eb28f0cc87c263d5f10570103361296c3569b1d
Izzy-Zoltan <izakzoltan@gmail.com>
2023-10-06 13:13:29 -0400

    10/6

---
+++ app.js
@@ -0,0 +1,15 @@
+const students = [{
+        firstname: "Gabriel Isaac",
+        Lastname: "Liberov"
+    },
+    {
+        firstname: "Noah",
+        lastname: "Rozin"
+    },
+    {
+        firstname: "Rylan",
+        lastname: "Geykhman"
+    }
+]
+
+students.forEach((students)=> console.log(students.firstname))
