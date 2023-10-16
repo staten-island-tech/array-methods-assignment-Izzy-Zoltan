5cce850e2de31dc73865f8347c37e1c2b6678184
Izzy-Zoltan <izakzoltan@gmail.com>
2023-10-16 13:00:38 -0400

    10/16

--- app.js
+++ app.js
@@ -1,43 +1,32 @@
-const numberlist = []
 const students = [{
         firstname: "Gabriel Isaac",
         Lastname: "Liberov",
         Haircolor: "\"Ginger\"",
         Brownhair:  false, 
-        number: [57,43]
     },
     {
         firstname: "Noah",
         lastname: "Rozin",
         Haircolor: "black",
         Brownhair: false,
-        number: [17,96,52]
     },
     {
         firstname: "Rylan",
         lastname: "Geykhman",
         Haircolor: "Brown",
         Brownhair: true,
-        number: [23]
     },
     {
         firstname: "Izzy",
         lastname: "Zoltan",
         Haircolor: "Brown-ER",
         Brownhair: true,
-        number: [89,69,42,100]
     }
 ]
 
-students.forEach((students)=> console.log(students.firstname))
-students.forEach((students)=> console.log(students))
-students.forEach((students)=> {
-    function numberchecker(number){
-            return number.filter(number>50);
-          }
-      
-    students.number.forEach((number)=> {
-    const numbers = students.number.filter(numberchecker(students.number));
-    numberlist.push(numbers)
-})})
-console.log(numberlist)
+students.forEach((student)=> console.log(student.firstname));
+students.forEach((student)=> console.log(student)) ;
+const browntrue = students.filter((student)=> student.Brownhair);
+browntrue.forEach((brownhaired)=> console.log(brownhaired.firstname))
+
+
