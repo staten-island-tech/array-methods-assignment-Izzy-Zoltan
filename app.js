4f9db36d6b481c9a76e09c2011f72b08bd5444b1
Izzy-Zoltan <izakzoltan@gmail.com>
2023-10-16 13:09:49 -0400

    10/16

--- app.js
+++ app.js
@@ -3,30 +3,36 @@ const students = [{
         Lastname: "Liberov",
         Haircolor: "\"Ginger\"",
         Brownhair:  false, 
+        num: [1,437],
     },
     {
         firstname: "Noah",
         lastname: "Rozin",
         Haircolor: "black",
         Brownhair: false,
+        num: [5],
     },
     {
         firstname: "Rylan",
         lastname: "Geykhman",
         Haircolor: "Brown",
         Brownhair: true,
+        num: [3,69,78],
     },
     {
         firstname: "Izzy",
         lastname: "Zoltan",
         Haircolor: "Brown-ER",
         Brownhair: true,
+        num:[8,42,36,12],
     }
-]
+];
 
 students.forEach((student)=> console.log(student.firstname));
-students.forEach((student)=> console.log(student)) ;
+students.forEach((student)=> console.log(student));
+students.forEach((student)=> 
+student.num.forEach((numbi)=> console.log(numbi)));
 const browntrue = students.filter((student)=> student.Brownhair);
-browntrue.forEach((brownhaired)=> console.log(brownhaired.firstname))
+browntrue.forEach((brownhaired)=> console.log(brownhaired.firstname));
 
 
